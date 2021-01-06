3215a36807faccfefd5852ce82878af0dadee109
MayXD <1096may1559629925@gmail.com>
2021-01-06 06:27:09 -0800

    vscode config

--- git_init.sh
+++ git_init.sh
@@ -3,7 +3,8 @@
 #git config --global user.name "MayXD"
 #git config --global user.email 1096may1559629925@gmail.com
 
+commit_dir=$(pwd)/*
 commit_info=$1
-git add ./*
+git add "$commit_dir"
 git commit -m "$commit_info"
 git push origin master
